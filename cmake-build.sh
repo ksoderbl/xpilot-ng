c18108cded71fbafb6bda9206296100294fc832a
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2026-04-10 00:38:26 +0300

    Enable many compiler warnings.

--- cmake-build.sh
+++ cmake-build.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 BUILD_DIR="${BUILD_DIR:-build}"
 
 cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release
-#cmake --build "$BUILD_DIR" -j"$(nproc)"
-cmake --build "$BUILD_DIR" -j"$(nproc)" --verbose
+cmake --build "$BUILD_DIR" -j"$(nproc)"
+#cmake --build "$BUILD_DIR" -j"$(nproc)" --verbose
 
